3da36571b59d020545ebf2586918afc17df8875b
James Peckham <jdpeckham@gmail.com>
2018-10-07 22:43:34 -0500

    fix debug log

--- mud.sh
+++ mud.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 while :
 do
+	#this cd makes the debug.log work.
+	cd /home/parnell/mud/mudlib
 	/usr/bin/driver /home/parnell/mud/mudlib/config.darke -d -fTEST1 -fTEST2
     echo "Press [CTRL+C] to stop.."
 	sleep 5s
