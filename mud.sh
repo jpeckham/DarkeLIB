c079741ebaef707726be9afe315be3a8eaeec587
James Peckham <jdpeckham@gmail.com>
2019-11-26 21:09:10 -0600

    drop debug spam/flags. add driver log

--- mud.sh
+++ mud.sh
@@ -3,7 +3,7 @@ while :
 do
 	#this cd makes the debug.log work.
 	cd /home/parnell/mud/mudlib
-	/usr/bin/driver /home/parnell/mud/mudlib/config.darke -d -fTEST1 -fTEST2
+	/usr/bin/driver /home/parnell/mud/mudlib/config.darke > /home/parnell/mud/mudlib/log/driver # -d -fTEST1 -fTEST2
     echo "Press [CTRL+C] to stop.."
 	sleep 5s
 done
